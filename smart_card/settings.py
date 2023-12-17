a501c675c10df7211904c0df0658f673c9d2e6c5
Mondesir <ridel.ntsoumou@topcenter.cg>
2023-12-17 17:31:56 +0100

    settings.py

--- smart_card/settings.py
+++ smart_card/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i)kxmx!r4l15%wg)xl2a5l)uwq*+eh!+ly#a-ko1*56oo5qi)#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
